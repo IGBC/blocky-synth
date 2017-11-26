7348f83c341a6cbafb27a13f5de078de986239d4
SEGFAULT <fakespamtrap@gmail.com>
2017-11-26 03:26:38 +0100

    yes I just dumped a LibGDX project into this thing, the idea is to use scene 2D to make the UI

---
+++ src/UI/java/desktop/src/com/blockysynth/ui/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.blockysynth.ui.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.blockysynth.ui.MainUI;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainUI(), config);
+	}
+}
